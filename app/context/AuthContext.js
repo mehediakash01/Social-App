188b9624241fa612510421d46933ccc2f92d4071
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-11-23 01:13:29 +0600

    intialize the project and added context api

---
+++ app/context/AuthContext.js
@@ -0,0 +1,29 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (firebaseUser) => {
+      setUser(firebaseUser || null);
+      setLoading(false);
+    });
+
+    return () => unsub();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
