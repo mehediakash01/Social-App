45e4f5de5e04d24bafab3bae5676363801fa8657
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-11-23 01:34:35 +0600

    added signin ,googlelogin and logout fuction in context api

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -1,29 +1,67 @@
 "use client";
 
-import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "@/lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useState, useEffect } from "react";
+import {
+  onAuthStateChanged,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup
+} from "firebase/auth";
+import { auth, googleProvider } from "@/firebase.config";
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
-export const AuthProvider = ({ children }) => {
+export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, async (firebaseUser) => {
-      setUser(firebaseUser || null);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser || null);
       setLoading(false);
     });
 
-    return () => unsub();
+    return () => unsubscribe();
   }, []);
 
+  // Email Sign Up
+  const signUpWithEmail = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Email Login
+  const loginWithEmail = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Google Login
+  const loginWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // Logout
+  const logoutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authValue = {
+    user,
+    loading,
+    signUpWithEmail,
+    loginWithEmail,
+    loginWithGoogle,
+    logoutUser,
+  };
+
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    <AuthContext.Provider value={authValue}>
       {children}
     </AuthContext.Provider>
   );
-};
-
-export const useAuth = () => useContext(AuthContext);
+}
